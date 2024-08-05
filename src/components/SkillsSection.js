7889789e2d284f4e22bc82f262cbf7899cb934a5
Armaan Nayyar <72308743+alphaninja27@users.noreply.github.com>
2024-08-05 10:12:00 +0530

    Update SkillsSection.js

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -6,8 +6,8 @@ const SkillsSection = () => {
     <Container>
       <Title>👨‍💻Skills</Title>
       <SectionList>
-        <ListItem>🖥️Languages: Python, SQL, MATLAB, C/C++</ListItem>
-        <ListItem>🛠️Tools: MS Excel, VS Code, GitHub, AWS, Azure, GCP, PowerBI</ListItem>
+        <ListItem>🖥️Languages: Python, SQL, MATLAB, C/C++, HTML/CSS</ListItem>
+        <ListItem>🛠️Tools: MS Excel, VS Code, GitHub, PowerBI, Cloud Platforms, Bugzilla, Ixia, Tmux</ListItem>
         <ListItem>🌐Frameworks: Django, Flask, React</ListItem>
         <ListItem>🗣Soft Skills: Public Speaking, Leadership, Event Management</ListItem>
       </SectionList>
