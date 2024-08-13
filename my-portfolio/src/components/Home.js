301e6a6517c8de19785b8307830bd32a4398c7b3
Armaan Nayyar <72308743+alphaninja27@users.noreply.github.com>
2024-08-13 22:48:10 +0530

    Update Home.js

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <section>
       <h2>Welcome to My Portfolio!</h2>
-      <img src="./my-portfolio/src/components/1614171945916.jpg" alt="Your Name" style={{ borderRadius: '50%', width: '150px', height: '150px' }} />
+      <img src="https://github.com/alphaninja27/Personal-Website/blob/c5c71a9ac2623fdcb3df735820336407a5c27b20/my-portfolio/src/components/1614171945916.jpg" alt="Your Name" style={{ borderRadius: '50%', width: '150px', height: '150px' }} />
       <p>Hi! I'm Armaan!, a passionate techie with an interest in various technologies. Welcome to my portfolio!</p>
     </section>
   );
