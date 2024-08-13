1470ea205a3491c6b66031568a969ac65f4fdb99
Armaan Nayyar <72308743+alphaninja27@users.noreply.github.com>
2024-08-13 23:29:42 +0530

    Update App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -80,7 +80,7 @@ function App() {
     home: (
       <section>
         <h2>Welcome to My Portfolio!</h2>
-        <img src="https://github.com/alphaninja27/Personal-Website/blob/c5c71a9ac2623fdcb3df735820336407a5c27b20/my-portfolio/src/components/1614171945916.jpg" alt="Your Name" style={{ borderRadius: '50%', width: '150px', height: '150px' }} />
+        <img src="./components/1614171945916.jpg" alt="Your Name" style={{ borderRadius: '50%', width: '150px', height: '150px' }} />
         <p>Hi! I'm Armaan!, a passionate techie with interest in various technologies. Welcome to my portfolio!</p>
       </section>
 
